92c1b7e62e5568a07b23a1a79d03133a3ba07c3f
stefanborcia <stefigabi89@yahoo.com>
2025-03-26 16:40:33 +0100

    Create Todo Model

---
+++ TodoAPI/Models/Todo.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoAPI.Models
+{
+    public class Todo
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; }
+        public DateTime IsComplete { get; set; }
+        public int Priority { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public Todo()
+        {
+            IsCompleted = false;
+        }
+    }
+}
