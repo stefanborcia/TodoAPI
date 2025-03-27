0d257e187f4219c95bc1c97f9381ec4bf973a093
stefanborcia <stefigabi89@yahoo.com>
2025-03-27 10:47:09 +0100

    Define an interface for service layer

--- TodoAPI/Program.cs
+++ TodoAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using TodoAPI.AppDataContext;
+using TodoAPI.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
+builder.Services.AddProblemDetails();
+
+// Adding of login 
+builder.Services.AddLogging();
 
 //Automapper
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
