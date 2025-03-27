fd47d5ccdbfb1679791910965b3815a1cc518cec
stefanborcia <stefigabi89@yahoo.com>
2025-03-27 11:15:52 +0100

    Implement the GetAllAsync &  CreateTodoAsyn in the TodoController

---
+++ TodoAPI/Controllers/TodoController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TodoAPI.Contracts;
+using TodoAPI.Interface;
+
+namespace TodoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly ITodoServices _todoServices;
+
+        public TodoController(ITodoServices todoServices)
+        {
+            _todoServices = todoServices;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            try
+            {
+                var todo = await _todoServices.GetAllAsync();
+                if (todo is null || !todo.Any())
+                {
+                    return Ok(new { message = "No Todo items found" });
+                }
+                return Ok(new { message = "Successfully retrieved Todo items", data = todo });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTodoAsync(CreateTodoRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new { message = "Invalid request" });
+            }
+
+            try
+            {
+                await _todoServices.CreateTodoAsync(request);
+                return Ok(new { message = "Todo item created successfully" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = ex.Message });
+            }
+        }
+    }
+}
