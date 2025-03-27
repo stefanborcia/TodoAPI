9b406ca6c0dbca579e668f61f2e40aa91e2f46f2
stefanborcia <stefigabi89@yahoo.com>
2025-03-27 11:00:36 +0100

    Implement the CreateTodoAsync & GetAllAsync Methods in the Service Class

---
+++ TodoAPI/Services/TodoServices.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using TodoAPI.AppDataContext;
+using TodoAPI.Contracts;
+using TodoAPI.Interface;
+using TodoAPI.Models;
+
+namespace TodoAPI.Services
+{
+    public class TodoServices : ITodoServices
+    {
+        private readonly TodoDbContext _context;
+        private readonly ILogger<TodoServices> _logger;
+        private readonly IMapper _mapper;
+
+        public TodoServices(TodoDbContext context, ILogger<TodoServices> logger, IMapper mapper)
+        {
+            _context = context;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        //  Create Todo for it be save in the datbase 
+        public async Task CreateTodoAsync(CreateTodoRequest request)
+        {
+            try
+            {
+                var todo = _mapper.Map<Todo>(request);
+                todo.CreatedAt = DateTime.UtcNow;
+                _context.Todos.Add(todo);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while creating the Todo item");
+                throw new Exception("An error occured while create the Todo item");
+            }
+        }
+
+        public Task DeleteTodoAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Get all Todo items from database
+        public async Task<IEnumerable<Todo>> GetAllAsync()
+        {
+            var todo = await _context.Todos.ToListAsync();
+            if(todo is null)
+            {
+                throw new Exception("No Todo items found");
+            }
+            return todo;
+        }
+
+        public Task<Todo?> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateTodoAsync(Guid id, UpdateTodoRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
