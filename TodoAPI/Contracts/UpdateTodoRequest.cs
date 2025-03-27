b0a875a32eaa46871dc1e02514e3ecbdda55546c
stefanborcia <stefigabi89@yahoo.com>
2025-03-27 10:26:50 +0100

    Add UpdateTodoRequest File

---
+++ TodoAPI/Contracts/UpdateTodoRequest.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoAPI.Contracts
+{
+    public class UpdateTodoRequest
+    {
+        [StringLength(100)]
+        public string? Title { get; set; }
+
+        [StringLength(500)]
+        public string? Description { get; set; }
+
+        public bool? IsCompleted { get; set; }
+        public DateTime? DueDate { get; set; }
+
+        [Range(1, 5)]
+        public int? Priority { get; set; }
+
+        public UpdateTodoRequest()
+        {
+            IsCompleted = false;
+        }
+    }
+}
